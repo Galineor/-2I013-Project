a9d742c964da7f0d20048a36b2bcdffc64f7bfcc
Galineor <gaspard.lunetta@gmail.com>
2017-03-20 22:06:41 +0100

    ROCK ON FIRE (no more)

--- src/Environnement/Terrain.java
+++ src/Environnement/Terrain.java
@@ -2,13 +2,13 @@ package Environnement;
 
 public class Terrain {
 	
-	public int type;
-	public int altitude;
-	public int water;
-	public boolean isTree;
+	public int type; //0= Plaine 1= Desert 2= Eau
+	public int altitude; //altitude de la case
+	public int water; //quantite d eau
+	public boolean isTree; //contient un arbre ??
 	private int AFA = 0; //0= Alive  1= Fire 2=Ashes
-	private boolean PheroPrey = false;
-	private boolean PheroPred = false;
+	private boolean PheroPrey = false; //une proie est passée par la ??
+	private boolean PheroPred = false; //un pred est passsé par la ???
 	
 	public Terrain(int type,int water, int alt, boolean tree){
 		this.type = type;
